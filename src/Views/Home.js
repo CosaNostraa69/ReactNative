73d1ad4f798041bad6abe35262ca8dc674ce2e66
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2023-12-01 11:58:18 +0100

    Add ReactNative Geolocation

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,7 +1,21 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import { HomeStyle } from "../Styles/Views/Home";
+import Geolocation from '@react-native-community/geolocation';
+import { useEffect, useState } from "react";
 
 const Home = ({ navigation }) => {
+    const [latitude, setLatitude] = useState("")
+    const [longitude, setLongitude] = useState("")
+
+    useEffect(() =>{
+        Geolocation.requestAuthorization()
+        Geolocation.getCurrentPosition((info) => {
+            setLatitude(info.coords.latitude)
+            setLongitude(info.coords.longitude)
+        });
+    
+    })
+
     return (
         <View style={HomeStyle.mainContainer}>
             <Text style={HomeStyle.mainTitle}>MonstrApp</Text>
@@ -11,6 +25,7 @@ const Home = ({ navigation }) => {
                 source={{ uri: 'https://images.unsplash.com/photo-1696246847440-bb0047ba93ca?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
                 style={HomeStyle.image}
             />
+            <Text> {` latitude :${latitude} longitude :${longitude}`}</Text>
 
             <TouchableOpacity
                 onPress={() => {
